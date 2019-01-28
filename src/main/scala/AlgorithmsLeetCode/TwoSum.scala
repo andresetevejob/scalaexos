799748cf90ad37cd4c54af727001a50de09817e2
andrestevejob <kouameandre43@gmail.com>
2019-01-28 03:13:37 +0100

    -Add Third approch solution -Correction v2 doc

--- src/main/scala/AlgorithmsLeetCode/TwoSum.scala
+++ src/main/scala/AlgorithmsLeetCode/TwoSum.scala
@@ -3,8 +3,8 @@ package AlgorithmsLeetCode
 object TwoSum {
 
     def bruteForce(tab:Array[Int],target:Int):Array[Int]={
-      for ( i <- 0 to (tab.length)) {
-        for ( j <- i+1 to (tab.length)) {
+      for ( i <- 0 to tab.length) {
+        for ( j <- i+1 to tab.length) {
             if(tab(i) + tab(j)== target){
                return Array(tab(i),tab(j))
             }
@@ -21,14 +21,28 @@ object TwoSum {
        }
        for(i<- 0 to tab.length){
           var complement = target - mapTab(i)
-          if(mapTab.contains(complement) && mapTab.get(complement)!=i){
+          var index : Int =  mapTab.getOrElse(complement,-1)
+          if(mapTab.contains(complement) && index!=i){
             Array(i,mapTab.get(complement))
           }
        }
        throw new IllegalArgumentException("No two sum solution")
     }
 
-    def main(args: Array[String])={
+    def hasTabSolutionTwo(tab: Array[Int],target:Int):Array[Int]={
+       var mapTab : Map[Int,Int] = Map()
+       for(i<- 0 to tab.length){
+           var complement : Int = target - tab(i)
+           if(mapTab.contains(complement)){
+              Array(mapTab.get(complement),i)
+           }else{
+             mapTab + (tab(i) -> i)
+           }
+       }
+      throw new IllegalArgumentException("No two sum solution")
+    }
+
+    def main(args: Array[String]): Unit ={
        val result = bruteForce(Array(2,7,8,10),12)
        println(result(0))
        println(result(1))
